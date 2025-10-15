9185c7cbd465155ebc9804423f2f87806b9b4975
falcaodiogo <falcao.diogo@ua.pt>
2025-10-15 20:47:37 +0100

    Update GitHub Actions workflow to include Husky and deploy to Netlify; add environment variables for Contentful

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  env: {
+    CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
+    CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
+    CONTENTFUL_PREVIEW_ACCESS_TOKEN: process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN,
+    CONTENTFUL_PREVIEW_SECRET: process.env.CONTENTFUL_PREVIEW_SECRET,
+    CONTENTFUL_REVALIDATE_SECRET: process.env.CONTENTFUL_REVALIDATE_SECRET,
+  },
   images: {
+    unoptimized: true,
     loader: "custom",
     formats: ["image/avif", "image/webp"],
   },
